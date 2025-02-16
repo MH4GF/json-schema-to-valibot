f5b2e526982231b8926f6aa179cd659d3b0e2cce
MH4GF <h.miyagi.cnw@gmail.com>
2025-02-16 12:55:15 +0900

    feat: update parseEnum to support mixed type arrays in schema

--- src/parsers/parseEnum.test.ts
+++ src/parsers/parseEnum.test.ts
@@ -3,20 +3,22 @@ import { describe, expect, it } from 'vitest'
 import { parseEnum } from './parseEnum.ts'
 
 describe('parseEnum', () => {
-  it('converts basic enum schema', () => {
+  it('converts mixed type array', () => {
     const schema: JSONSchema4 = {
-      enum: ['a', 'b', 'c'],
+      enum: ['a', 1, true, null],
     }
-    expect(parseEnum(schema, {})).toMatchInlineSnapshot('"v.picklist(["a","b","c"])"')
+    expect(parseEnum(schema, {})).toMatchInlineSnapshot(
+      `"v.union([v.literal("a"),v.literal(1),v.literal(true),v.null()])"`,
+    )
   })
 
   it('handles default value', () => {
     const schema: JSONSchema4 = {
-      enum: ['a', 'b', 'c'],
+      enum: ['a', 1, true, null],
       default: 'a',
     }
     expect(parseEnum(schema, {})).toMatchInlineSnapshot(
-      '"v.optional(v.picklist(["a","b","c"]), \'a\')"',
+      `"v.optional(v.union([v.literal("a"),v.literal(1),v.literal(true),v.null()]), 'a')"`,
     )
   })
 })
--- src/parsers/parseEnum.ts
+++ src/parsers/parseEnum.ts
@@ -1,14 +1,19 @@
-import type { JSONSchema4, JSONSchema4TypeName } from 'json-schema'
+import type { JSONSchema4 } from 'json-schema'
 import type { Options } from '../types.ts'
 import { withDefault } from '../utils/withDefault.ts'
 import { withDescription } from '../utils/withDescription.ts'
 
 export function parseEnum(schema: JSONSchema4, options: Options = {}): string {
-  const values = schema.enum as Array<string | number>
-  const picklistSchema = `v.picklist([${values.map((value) => JSON.stringify(value)).join(',')}])`
+  const literals =
+    schema.enum?.map((value) => {
+      if (value === null) {
+        return 'v.null()'
+      }
+      return `v.literal(${JSON.stringify(value)})`
+    }) ?? []
 
-  const schemaWithType = { ...schema, type: 'string' as JSONSchema4TypeName }
-  const baseSchema = withDefault(schemaWithType, picklistSchema, options)
+  const unionSchema = `v.union([${literals.join(',')}])`
+  const baseSchema = withDefault(schema, unionSchema, options)
 
   return withDescription(schema, baseSchema, options)
 }
